a648106f9a368d6f729b06b75fbbc42fd3efd25f
sh48846 <sakib.hasan@provpsm.com>
2025-09-03 16:15:46 -0500

    Update Toolbox 2016-12-02.js

--- Toolbox 2016-12-02.js
+++ Toolbox 2016-12-02.js
@@ -16,7 +16,7 @@ var jsJqueryUI = 'https://www.costestimatorpro.com/content/rstools/js/jquery-ui.
 
 // stylesheet locations
 //var toolboxCSS = 'https://rawgit.com/dvargas46/provenance/master/iPRSM%20Toolbox.css';
-var toolboxCSS = 'https://cdn.jsdelivr.net/gh/sh48846/Tools@b0441dd/Toolbox.min.css';
+var toolboxCSS = 'https://cdn.jsdelivr.net/gh/sh48846/Tools@cbd11c2/Toolbox.min.css';
 
 // image locations
 // Array of images with their weights (probabilities)
@@ -1237,6 +1237,7 @@ function ProgressBar() {
         $('.post__Go').toggleClass('post__Go--show',100);
     }
 }
+
 
 
 
